40adae40e144143a4c3e12a9f4b96d5e2bb25155
Pavel Korolev <dev@borodust.org>
2021-08-29 16:39:21 +0300

    Fix darwin bindings

--- src/claw.lisp
+++ src/claw.lisp
@@ -5,7 +5,7 @@
                   (:exclude-definitions "FILE_LINE")
                   (:targets ((:and :x86-64 :linux) "x86_64-pc-linux-gnu")
                             ((:and :x86-64 :windows) "x86_64-w64-mingw32")
-                            ((:and :x86-64 :drawin) "x86_64-apple-darwin-gnu"))
+                            ((:and :x86-64 :darwin) "x86_64-apple-darwin-gnu"))
                   (:persistent :bodge-nuklear-bindings
                    :depends-on (:claw-utils)))
   :in-package :%nuklear
